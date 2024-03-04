817ffdfd8e66331cee049e4ca9eac05ef5d0ca66
Leah Deshar <leah.desar@gmail.com>
2024-03-04 12:00:35 +0545

    add reservation ui

---
+++ cozynook/app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-4 px-6 cursor-pointer bg-cozynook text-white rounded-xl hover:bg-cozynook-dark transition">
+      Contact
+    </div>
+  );
+};
+
+export default ContactButton;
