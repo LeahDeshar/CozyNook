9b51ad8eb00c45be9a91872d97c50467996cfcf9
Leah Deshar <leah.desar@gmail.com>
2024-03-04 12:07:22 +0545

    add inbox ui

---
+++ cozynook/app/components/inbox/Conversations.tsx
@@ -0,0 +1,11 @@
+const Conversations = () => {
+  return (
+    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+      <p className="mb-6 text-xl">John Doe</p>
+
+      <p className="text-airbnb-dark">Go to conversation</p>
+    </div>
+  );
+};
+
+export default Conversations;
