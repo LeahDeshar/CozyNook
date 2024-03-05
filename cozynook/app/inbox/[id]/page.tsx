140b48a12d27d7c9be619bf0554b46037b837800
Leah Deshar <leah.desar@gmail.com>
2024-03-05 15:24:59 +0545

    converstaion

--- cozynook/app/inbox/[id]/page.tsx
+++ cozynook/app/inbox/[id]/page.tsx
@@ -1,15 +1,12 @@
 // import Conversation from "../components/inbox/Conversation";
+// import ConversationDetail from "@/app/components/inbox/ConversationDetail";
 
-const InboxPage = () => {
+const ConversationPage = () => {
   return (
-    <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
-      <h1 className="my-6 text-2xl">Inbox</h1>
-
-      {/* <Conversation />
-            <Conversation />
-            <Conversation /> */}
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      {/* <ConversationDetail /> */}
     </main>
   );
 };
 
-export default InboxPage;
+export default ConversationPage;
---
+++ cozynook/app/inbox/page.tsx
@@ -0,0 +1,15 @@
+import Conversations from "../components/inbox/Conversations";
+
+const InboxPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
+      <h1 className="my-6 text-2xl">Inbox</h1>
+
+      <Conversations />
+      <Conversations />
+      <Conversations />
+    </main>
+  );
+};
+
+export default InboxPage;
